f59faba7d735958b315e65a1dd48701d521fe43c
Dom B <domlamont@yahoo.com>
2021-11-17 14:50:55 -0800

    Created a function that updates the state book number variable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,24 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Ready Player One", author: "Earnest"},
+            {title: "Starry Night", author: "Bob"}
+        ];
+        this.state = {
+            bookNumber: 0
+        };
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render(){
@@ -13,6 +30,18 @@ class App extends Component{
             <div>    
                 <div className='titlebar-nav'>
                     <TitleBar className='container-fluid' />
+                </div>
+
+                <div className="col-md-4">
+                    <h1>First Div</h1>
+                </div>
+
+                <div className="col-md-4">
+                    <h2>Second Div</h2>
+                </div>
+
+                <div className="col-md-4">
+                    <h2>Third Div</h2>
                 </div>
             </div>
         );
